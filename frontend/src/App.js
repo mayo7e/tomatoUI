eb520d1eade0c77cb2fd500c15dacd886b6b8423
mayo7e <mayowa.wh@gmail.com>
2022-12-30 14:51:20 +0800

    Ad- header section

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import Header from "./header.js"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+          <h2>Monitor Erroe and <span>User Interaction</span></h2>
+          <h3>A Javascript client-side library for building error logs and user interaction trace dataset </h3>
+          <div className="form_grp">
+            <form>
+              <input />
+            </form>
+            <button>request access</button>
+          </div>
+      </main>
+      <footer>
+        Copyright &copy; 2022
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/header.js
@@ -0,0 +1,31 @@
+
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+        
+            <header>
+                <div className="logo" >
+                    <h1><span>tomato</span>UI</h1>
+                </div>
+                <nav>
+                    <ul  className="nav-pg-01">
+                        <li>Docs</li>
+                        <li>Blog</li>
+                        <li>Community</li>
+                    </ul>
+                
+
+                    <ul  className="nav-pg-02">
+                        <li>Github</li>
+                        <li>twitter</li>
+                    </ul>
+                </nav>
+            </header>
+
+    </div>
+  )
+}
+
+export default header
